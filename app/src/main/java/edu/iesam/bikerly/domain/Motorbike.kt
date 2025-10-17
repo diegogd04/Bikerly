6dc65b48da78b26303ab67ddb77d8357e4bf8363
Diego González Díaz <diego2612004@gmail.com>
2025-10-17 16:21:28 +0200

    Crear modelo de datos Motorbike.

---
+++ app/src/main/java/edu/iesam/bikerly/domain/Motorbike.kt
@@ -0,0 +1,10 @@
+package edu.iesam.bikerly.domain
+
+data class Motorbike(
+    val id: String,
+    val make: String,
+    val model: String,
+    val year: String,
+    val type: String,
+    val displacement: String
+)
